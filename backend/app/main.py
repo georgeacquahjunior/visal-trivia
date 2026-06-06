24e54aa2ccbf9f6f9a1a6f43eeb9013a282b2164
georgeacquahjunior <georgeacquahjunior@gmail.com>
2026-06-06 09:23:45 +0000

    feat: added leaderboard in admin view

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import sentry_sdk
 
 from app.api.routes import router
 from app.core.config import settings
+
+sentry_sdk.init(
+    dsn="https://72abdf845d19142e32502fd97d3c3aa5@o4511507821101056.ingest.de.sentry.io/4511507965280336",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 
 app = FastAPI(title=settings.app_name)
 
